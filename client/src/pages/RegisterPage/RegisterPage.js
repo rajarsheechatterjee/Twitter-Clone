bf8837fd06a9b8b8a03035cc4ae68ad0213b0823
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2020-07-12 21:19:38 +0530

    RegisterPageStyles

--- client/src/pages/RegisterPage/RegisterPage.js
+++ client/src/pages/RegisterPage/RegisterPage.js
@@ -5,6 +5,7 @@ import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
+import './RegisterPageStyles.css';
 
 const RegisterPage = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
@@ -34,34 +35,45 @@ const RegisterPage = ({ setAlert, register, isAuthenticated }) => {
 
     return (
         <Fragment>
-            <h1>Create your account</h1>
+            <div className='r-1x0uki6 r-1ye8kvj r-13qz1uu r-1j3t67a r-1jgb5lz'>
+            <h1 className='r-6ity3w r-15d164r r-1blvdjr r-jwli3a r-vw2c0b'>Create your account</h1>
             <form onSubmit={e => onSubmit(e)} >
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Name</label>
+              <div className="form-group my-2">
+              <label className=' r-wgabs5  r-1uaug3w r-18u37iz r-rull8r  r-1778zho r-1jkafct css-1dbjc4n' htmlFor="Name">
+                  <span className='r-glunga r-utggzx r-k200y r-16dba41 r-111h2gw r-a023e6'>Name</span>
+                </label>
                 <input name='name' type="text" className="form-control" value={name} onChange={e => onChange(e)} />
               </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Username</label>
+              <div className="form-group my-2">
+              <label className=' r-wgabs5  r-1uaug3w r-18u37iz r-rull8r  r-1778zho r-1jkafct css-1dbjc4n' htmlFor="Username">
+                  <span className='r-glunga r-utggzx r-k200y r-16dba41 r-111h2gw r-a023e6'>Username</span>
+                </label>
                 <input name='username' type="text" className="form-control" value={username} onChange={e => onChange(e)} />
               </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
+              <div className="form-group my-2">
+              <label className=' r-wgabs5  r-1uaug3w r-18u37iz r-rull8r  r-1778zho r-1jkafct css-1dbjc4n' htmlFor="Email">
+                  <span className='r-glunga r-utggzx r-k200y r-16dba41 r-111h2gw r-a023e6'>Email Address</span>
+                </label>
                 <input name='email' type="email" className="form-control" value={email} onChange={e => onChange(e)} />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                <small id="emailHelp" style={{color: '#8899a6'}} className="form-text">We'll never share your email with anyone else.</small>
               </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
+              <div className="form-group my-2">
+              <label className=' r-wgabs5  r-1uaug3w r-18u37iz r-rull8r  r-1778zho r-1jkafct css-1dbjc4n' htmlFor="Password">
+                  <span className='r-glunga r-utggzx r-k200y r-16dba41 r-111h2gw r-a023e6'>Password</span>
+                </label>
                 <input name='password' type="password" minLength='6' className="form-control" value={password} onChange={e => onChange(e)} />
               </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword2">Confirm Password</label>
-                <input name='password2' type="password" minLength='6' className="form-control" value={password2} onChange={e => onChange(e)} />
+              <div className="form-group my-2">
+              <label className=' r-wgabs5  r-1uaug3w r-18u37iz r-rull8r  r-1778zho r-1jkafct css-1dbjc4n' htmlFor="ConfPass">
+                  <span className='r-glunga r-utggzx r-k200y r-16dba41 r-111h2gw r-a023e6'>Confirm Password</span>
+                </label>                <input name='password2' type="password" minLength='6' className="form-control" value={password2} onChange={e => onChange(e)} />
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" style={{ width: '100%', cursor:'pointer' }}  className="mx-0 my-4 r-17bavie r-1jayybb r-urgr8i  r-1ny4l3l r-6416eg  r-1w2pmg  r-o7ynqc  r-lrvibr  r-15bsvpr r-vlx1xi  r-zg41ew  r-42olwf  r-rs99b7  r-1phboty r-sdzlij r-jwli3a  r-eljoum  r-vw2c0b  r-a023e6  r-1qd0xha r-qvutc0  r-bcqeeo  r-1awozwy r-1777fci r-16y2uox r-18u37iz r-q4m81j  r-dnmrzs  r-6koalj ">Submit</button>
             </form>
-            <p className="my-1">
+            <p className="my-1 text-light" style={{ textAlign: 'center' }} >
                 Already have an account? <Link to='/login'>Login</Link>
             </p>
+          </div>
         </Fragment>
     )
 }
