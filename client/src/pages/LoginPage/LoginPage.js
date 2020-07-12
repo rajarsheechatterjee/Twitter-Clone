5c81b8a16c0a3cfe3d0134f1709f4d75e27f0ff1
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2020-07-12 20:35:41 +0530

    Login UI Fix

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
 
+import './LoginPageStyles.css';
 
 const LoginPage = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
@@ -26,21 +27,28 @@ const LoginPage = ({ login, isAuthenticated }) => {
 
     return (
         <Fragment>
-            <h1>Login</h1>
+          <div className='r-1x0uki6 r-1ye8kvj r-13qz1uu r-1j3t67a r-1jgb5lz'>
+          <h1 className='r-6ity3w r-15d164r r-1blvdjr r-jwli3a r-vw2c0b' >Login in to Twitter</h1>
             <form onSubmit={e => onSubmit(e)} >
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
+              <div className="form-group my-2">
+                <label className=' r-wgabs5  r-1uaug3w r-18u37iz r-rull8r  r-1778zho r-1jkafct css-1dbjc4n' htmlFor="exampleInputEmail1">
+                  <span className='r-glunga r-utggzx r-k200y r-16dba41 r-111h2gw r-a023e6'>Email Address</span>
+                </label>
                 <input name='email' type="email" className="form-control" value={email} onChange={e => onChange(e)} required />
               </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
+              <div className="form-group my-2">
+                <label className=' r-wgabs5  r-1uaug3w r-18u37iz r-rull8r  r-1778zho r-1jkafct css-1dbjc4n' htmlFor="exampleInputEmail1">
+                  <span className='r-glunga r-utggzx r-k200y r-16dba41 r-111h2gw r-a023e6'>Password</span>
+                </label>
                 <input name='password' type="password" minLength='6' className="form-control" value={password} onChange={e => onChange(e)} required />
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" style={{ width: '100%' }}  className="mx-0 my-4 r-17bavie r-1jayybb r-urgr8i  r-1ny4l3l r-6416eg  r-1w2pmg  r-o7ynqc  r-lrvibr  r-15bsvpr r-vlx1xi  r-zg41ew  r-42olwf  r-rs99b7  r-1phboty r-sdzlij r-jwli3a  r-eljoum  r-vw2c0b  r-a023e6  r-1qd0xha r-qvutc0  r-bcqeeo  r-1awozwy r-1777fci r-16y2uox r-18u37iz r-q4m81j  r-dnmrzs  r-6koalj "  >Submit</button>
             </form>
-            <p className="my-1">
+            <p className="my-1 text-light" style={{ textAlign: 'center' }} >
                 Dont have an account? <Link to='/register'>Sign up</Link>
             </p>
+          </div>
+            
         </Fragment>
     )
 }
